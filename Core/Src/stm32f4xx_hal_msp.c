e2445afc3e94d7cb68b4ad6ec4a1b8ac6ef9b198
mikeshop1008 <mikeshop1008@gmail.com>
2026-02-26 16:52:06 +0800

    Initial import from Keil Studio Cloud

---
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -0,0 +1,9 @@
+#include "stm32f4xx_hal.h"
+
+void HAL_MspInit(void)
+{
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
+    __HAL_RCC_PWR_CLK_ENABLE();
+
+    HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+}
